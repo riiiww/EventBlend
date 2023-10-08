59d6d1ec46a29292aa68504018de2d80dfcf8611
riiiww <fotan.renata@student.uzhnu.edu.ua>
2023-10-08 22:43:29 +0300

    Create header

---
+++ src/App.js
@@ -0,0 +1,9 @@
+import Header from "../src/page/header"
+function App() {
+  
+  return (
+    <Header/>
+  );
+}
+
+export default App;
---
+++ src/page/header.jsx
@@ -0,0 +1,58 @@
+import './header.css'
+import search_icon from "../png/icons/search-icon.svg"
+import calendar_icon from "../png/icons/calendar-icon.svg"
+import sity_icon from "../png/icons/sity-svg.svg"
+
+
+function Header () {
+    return(
+        <div className="container">
+            <header className='header'>
+         <div class="header__wrapper">
+            <div class="header__block1">
+          <div class="logo">
+            Event Blend
+          </div>
+          <div class="logo2">
+            Event Blend
+          </div>
+        </div>
+        <div class="header__block2">
+          <nav class="nav">
+            <a href="/">
+              Головна
+            </a>
+            <a href="/">
+              Події
+            </a>
+            <a href="/">
+              Квитки
+            </a>
+          </nav>
+        </div>
+        <div class="header__block3">
+          <a href="/">
+            <img src={search_icon} alt="search"></img>
+          </a>
+          <a href="/">
+            <img src={calendar_icon} alt="calendar"></img>
+          </a>
+        </div>
+        <div class="header__block4">
+          <a href="/">
+            <img src={sity_icon} alt="sity"></img>
+          </a>
+          <a href="/">
+            Оберіть місто
+          </a>
+          <a href="/">
+            Увійти
+          </a>
+        </div>
+            </div>
+        </header>
+
+        </div>
+    )
+}
+export default Header;
