83f04dbc705621565cb87865e5a00869487f0ef0
riiiww <fotan.renata@student.uzhnu.edu.ua>
2023-12-04 09:21:55 +0200

    Fix page "Tickets.jsx"

--- src/page/Tickets.jsx
+++ src/page/Tickets.jsx
@@ -4,16 +4,27 @@ import TemporaryName from '../png/pictures/ads.jpg'
 
 function Tickets() {
     return (
-        <header className='TicketsHeader'> 
-            <div className='TicketsContainer'>
-                <Link to="/CreateAds" style={{textDecoration: 'none'}}><button className="ButtonAddAds">Додати рекламу</button></Link>
+        <header className='headerTickets'> 
+            <div className='containerTickets'>
+                <a href="/">
+                    <Link to="/CreateAds"><button className="ButtonAddAds">Додати рекламу</button></Link>
+                </a>
                 <div className='adsBlock'>
                     <div className='adsOne'>
                         <div className='adsInfo'>
                             <img src={TemporaryName} alt="adsImage" />
                             <div className='adsDescription'>
                                 <h1 className="adsText">TemporaryName</h1>
-                                <h1 className="adsdescription">Енергійний концерт, що змішує виразність музики та потужний виступ. Відчуйте атмосферу неймовірної гармонії та емоційного підйому. Непередбачувана подорож у світ звуку.</h1>
+                                <h1 className="description">Енергійний концерт, що змішує виразність музики та потужний виступ. Відчуйте атмосферу неймовірної гармонії та емоційного підйому. Непередбачувана подорож у світ звуку.</h1>
+                            </div>
+                        </div>
+                    </div>
+                    <div className='adsOne'>
+                        <div className='adsInfo'>
+                            <img src={TemporaryName} alt="adsImage" />
+                            <div className='adsDescription'>
+                                <h1 className="adsText">TemporaryName</h1>
+                                <h1 className="description">Енергійний концерт, що змішує виразність музики та потужний виступ. Відчуйте атмосферу неймовірної гармонії та емоційного підйому. Непередбачувана подорож у світ звуку.</h1>
                             </div>
                         </div>
                     </div>
