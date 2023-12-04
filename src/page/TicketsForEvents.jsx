2c92d1346b78946fbec932b2f2573494d0727df1
riiiww <fotan.renata@student.uzhnu.edu.ua>
2023-12-04 16:22:33 +0200

    Updated page "TicketsForEvents"

--- src/page/TicketsForEvents.jsx
+++ src/page/TicketsForEvents.jsx
@@ -1,37 +1,44 @@
-import './ticketsForEvents.css'
+import React, { useRef } from 'react';
+import './ticketsForEvents.css';
 import { Link } from 'react-router-dom';
-import TemporaryName from '../png/pictures/ads.jpg'
+import ads from '../png/pictures/ads.png';
 
 function TicketsForEvents() {
+    const adsBlockRef = useRef(null);
+
+    const scrollRight = () => {
+        if (adsBlockRef.current) {
+            adsBlockRef.current.scrollLeft += 1935;
+        }
+    };
+
+    const scrollLeft = () => {
+        if (adsBlockRef.current) {
+            adsBlockRef.current.scrollLeft -= 1935;
+        }
+    };
+
     return (
-        <header className='headerTickets'> 
+        <header className='headerTickets'>
             <div className='containerTickets'>
-                <a href="/">
-                    <Link to="/CreateAds"><button className="ButtonAddAds">Додати рекламу</button></Link>
-                </a>
-                <div className='adsBlock'>
-                    <div className='adsOne'>
-                        <div className='adsInfo'>
-                            <img src={TemporaryName} alt="adsImage" />
-                            <div className='adsDescription'>
-                                <h1 className="adsText">TemporaryName</h1>
-                                <h1 className="description">Енергійний концерт, що змішує виразність музики та потужний виступ. Відчуйте атмосферу неймовірної гармонії та емоційного підйому. Непередбачувана подорож у світ звуку.</h1>
-                            </div>
-                        </div>
+                <Link to="/CreateAds" style={{ textDecoration: 'none' }}>
+                    <button className="ButtonAddAds">Додати рекламу</button>
+                </Link>
+                <div className='adsBlock' ref={adsBlockRef}>
+                    <div className='scrollButtons'>
+                        <button style={{width: '50px', height: '50px'}} className="scrollLeftButton" onClick={scrollLeft}></button>
+                        <button style={{width: '50px', height: '50px'}} className="scrollRightButton" onClick={scrollRight}></button>
                     </div>
                     <div className='adsOne'>
-                        <div className='adsInfo'>
-                            <img src={TemporaryName} alt="adsImage" />
-                            <div className='adsDescription'>
-                                <h1 className="adsText">TemporaryName</h1>
-                                <h1 className="description">Енергійний концерт, що змішує виразність музики та потужний виступ. Відчуйте атмосферу неймовірної гармонії та емоційного підйому. Непередбачувана подорож у світ звуку.</h1>
-                            </div>
-                        </div>
+                        <img src={ads} alt="adsImage" className="adsImage" />
+                    </div>
+                    <div className='adsOne'>
+                        <img src={ads} alt="adsImage" className="adsImage" />
                     </div>
                 </div>
             </div>
         </header>
-    )
+    );
 }
 
 export default TicketsForEvents;
