ad7ad8b6eb21c1da7c01976eac0e2429b6888df7
riiiww <fotan.renata@student.uzhnu.edu.ua>
2023-12-26 22:58:39 +0200

    Updated page TicketsForEvents

--- src/page/TicketsForEvents.jsx
+++ src/page/TicketsForEvents.jsx
@@ -42,24 +42,44 @@ function TicketsForEvents() {
 
         return () => clearInterval(intervalId);
     }, []); 
-
-    return (
+    if(setAdImages){
+        return (
+            <div className='headerTickets'>
+                <div className='containerTickets'>
+                    {isAuthenticated && isOrganizer && <Link to="/CreateAds" style={{ textDecoration: 'none' }}>
+                        <button className="ButtonAddAds">Додати рекламу</button>
+                    </Link>}
+                    <div className='advert' ref={adsBlockRef}>
+                        <div className='scrollButtons'>
+                            <button style={{ width: '50px', height: '50px' }} className="scrollLeftButton" onClick={scrollLeft}></button>
+                            <button style={{ width: '50px', height: '50px' }} className="scrollRightButton" onClick={scrollRight}></button>
+                        </div>
+                        {adImages.map((imageUrl, index) => (
+                            <div key={index} className='advert'>
+                                <img src={imageUrl} alt={`adsImage-${index}`} className="adsImage" />
+                            </div>
+                        ))}
+                    </div>
+                    <div className='contTickets'>
+                        <div className='lll'><img style={{width: '200px', height: '200px'}}src={tik} alt="adsImage" className="adsImage" /></div>
+                        <div className='lll'><img style={{width: '200px', height: '200px'}}src={tik} alt="adsImage" className="adsImage" /></div>
+                        <div className='lll'><img style={{width: '200px', height: '200px'}}src={tik} alt="adsImage" className="adsImage" /></div>
+                        <div className='lll'><img style={{width: '200px', height: '200px'}}src={tik} alt="adsImage" className="adsImage" /></div>
+                        <div className='lll'><img style={{width: '200px', height: '200px'}}src={tik} alt="adsImage" className="adsImage" /></div>
+                        <div className='lll'><img style={{width: '200px', height: '200px'}}src={tik} alt="adsImage" className="adsImage" /></div>
+                        <div className='lll'><img style={{width: '200px', height: '200px'}}src={tik} alt="adsImage" className="adsImage" /></div>
+                        <div className='lll'><img style={{width: '200px', height: '200px'}}src={tik} alt="adsImage" className="adsImage" /></div>
+                    </div>
+                </div>
+            </div>
+        );
+    }else{
+        return (
         <div className='headerTickets'>
             <div className='containerTickets'>
                 {isAuthenticated && isOrganizer && <Link to="/CreateAds" style={{ textDecoration: 'none' }}>
                     <button className="ButtonAddAds">Додати рекламу</button>
                 </Link>}
-                <div className='advert' ref={adsBlockRef}>
-                    <div className='scrollButtons'>
-                        <button style={{ width: '50px', height: '50px' }} className="scrollLeftButton" onClick={scrollLeft}></button>
-                        <button style={{ width: '50px', height: '50px' }} className="scrollRightButton" onClick={scrollRight}></button>
-                    </div>
-                    {adImages.map((imageUrl, index) => (
-                        <div key={index} className='adsOne'>
-                            <img src={imageUrl} alt={`adsImage-${index}`} className="adsImage" />
-                        </div>
-                    ))}
-                </div>
                 <div className='contTickets'>
                     <div className='lll'><img style={{width: '200px', height: '200px'}}src={tik} alt="adsImage" className="adsImage" /></div>
                     <div className='lll'><img style={{width: '200px', height: '200px'}}src={tik} alt="adsImage" className="adsImage" /></div>
@@ -72,7 +92,8 @@ function TicketsForEvents() {
                 </div>
             </div>
         </div>
-    );
+    );}
+    
 }
 
 export default TicketsForEvents;
